4d7cba21e6d948aa61e8eee17ac5ce2c8f4f58d5
chucky <89501967+KIRANRAJ6554@users.noreply.github.com>
2021-10-20 09:44:19 +0530

    Update __google.py

--- TGNRobot/modules/__google.py
+++ TGNRobot/modules/__google.py
@@ -275,7 +275,7 @@ async def apk(e):
         await e.reply("Exception Occured:- " + str(err))
 
 
-__mod_name__ = "◎Search"
+__mod_name__ = "🤑Search🤑"
 
 __help__ = """
  ❍ /google <text>*:* Perform a google search

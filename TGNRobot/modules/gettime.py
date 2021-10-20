7dedfc133130e729194b70614571e1509be5d4a3
chucky <89501967+KIRANRAJ6554@users.noreply.github.com>
2021-10-20 08:49:52 +0530

    Update gettime.py

--- TGNRobot/modules/gettime.py
+++ TGNRobot/modules/gettime.py
@@ -95,6 +95,6 @@ TIME_HANDLER = DisableAbleCommandHandler("time", gettime)
 
 dispatcher.add_handler(TIME_HANDLER)
 
-__mod_name__ = "TIME"
+__mod_name__ = "🕰️TIME🕰️"
 __command_list__ = ["time"]
 __handlers__ = [TIME_HANDLER]

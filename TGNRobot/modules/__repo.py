781085a8a7c950869be6ba5be01e92b95c00d34c
ValtAoiTheBot <91861970+ValtAoiTheBot@users.noreply.github.com>
2021-10-21 22:43:36 +0530

    Update __repo.py

--- TGNRobot/modules/__repo.py
+++ TGNRobot/modules/__repo.py
@@ -5,14 +5,14 @@ from pyrogram.types import *
 from TGNRobot.conf import get_str_key
 from TGNRobot import pbot
 
-REPO_TEXT = "**A Powerful [BOT](https://telegra.ph/file/cab6825dea9263d347831.jpg) to Make Your Groups Secured and Organized ! \n\n↼ Øwñêr ⇀ : 『 [Akki](t.me/godfatherakki) 』\n╭──────────────\n┣─ » Python ~ 3.8.6\n┣─ » Update ~ Recently\n╰──────────────\n\n»»» @CHUCKYMUSIC_BOT «««"
+REPO_TEXT = "**A Powerful [BOT](https://telegra.ph/file/cab6825dea9263d347831.jpg) to Make Your Groups Secured and Organized ! \n\n↼ Øwñêr ⇀ : 『 [kittu](t.me/Kittu_the_criminal) 』\n╭──────────────\n┣─ » Python ~ 3.8.6\n┣─ » Update ~ Recently\n╰──────────────\n\n»»» @CHUCKYMUSIC_BOT «««"
   
 BUTTONS = InlineKeyboardMarkup(
       [[
         InlineKeyboardButton("⚡Owner 🔥", url="t.me/Kittu_the_criminal"),
-        InlineKeyboardButton(" ᴊᴏɪɴ 💫", url="https://t.me/Superior_Bots"),
+        InlineKeyboardButton(" ᴊᴏɪɴ 💫", url="https://t.me/VALTAOITHEBOT"),
       ],[
-        InlineKeyboardButton("ᴛɢɴ ᴏᴡɴᴇʀ ❣️", url="https://t.me/chuckmusic"),
+        InlineKeyboardButton("Friend ❣️", url="https://t.me/Thanimaibot"),
         InlineKeyboardButton("ꜱᴜᴘᴘᴏʀᴛ ⚡", url="https://t.me/chuckmusic"),
       ],[
         InlineKeyboardButton("⚡ ᴜᴘᴅᴀᴛᴇꜱ ☑️", url="https://t.me/chuckmusicupdate"),

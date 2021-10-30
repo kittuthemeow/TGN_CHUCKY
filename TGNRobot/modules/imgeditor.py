a03c468d7b67e794286d0a97c00a6c43b391d16b
Kittuthemeow <89501967+KIRANRAJ6554@users.noreply.github.com>
2021-10-30 06:50:58 -0700

    Update imgeditor.py

--- TGNRobot/modules/imgeditor.py
+++ TGNRobot/modules/imgeditor.py
@@ -10,7 +10,7 @@ from pyrogram.types import (
 )
 
 # By @TroJanzHEX
-from SophiaBot.Addons.ImageEditor.edit_1 import (  # pylint:disable=import-error
+from TGNRobot.Addons.ImageEditor.edit_1 import (  # pylint:disable=import-error
     black_white,
     box_blur,
     bright,
@@ -18,7 +18,7 @@ from SophiaBot.Addons.ImageEditor.edit_1 import (  # pylint:disable=import-error
     mix,
     normal_blur,
 )
-from SophiaBot.Addons.ImageEditor.edit_2 import (  # pylint:disable=import-error
+from TGNRobot.Addons.ImageEditor.edit_2 import (  # pylint:disable=import-error
     cartoon,
     circle_with_bg,
     circle_without_bg,
@@ -28,13 +28,13 @@ from SophiaBot.Addons.ImageEditor.edit_2 import (  # pylint:disable=import-error
     sepia_mode,
     sticker,
 )
-from SophiaBot.Addons.ImageEditor.edit_3 import (  # pylint:disable=import-error
+from TGNRobot.Addons.ImageEditor.edit_3 import (  # pylint:disable=import-error
     black_border,
     blue_border,
     green_border,
     red_border,
 )
-from SophiaBot.Addons.ImageEditor.edit_4 import (  # pylint:disable=import-error
+from TGNRobot.Addons.ImageEditor.edit_4 import (  # pylint:disable=import-error
     inverted,
     removebg_plain,
     removebg_sticker,
@@ -44,7 +44,7 @@ from SophiaBot.Addons.ImageEditor.edit_4 import (  # pylint:disable=import-error
     rotate_270,
     round_sticker,
 )
-from SophiaBot.Addons.ImageEditor.edit_5 import (  # pylint:disable=import-error
+from TGNRobot.Addons.ImageEditor.edit_5 import (  # pylint:disable=import-error
     normalglitch_1,
     normalglitch_2,
     normalglitch_3,
@@ -56,7 +56,7 @@ from SophiaBot.Addons.ImageEditor.edit_5 import (  # pylint:disable=import-error
     scanlineglitch_4,
     scanlineglitch_5,
 )
-from SophiaBot.pyrogramee.pyrogram import Sophia as Client
+from TGNRobot.pyrogramee.pyrogram import Sophia as Client
 
 lel = 00000000
 # pylint:disable=import-error

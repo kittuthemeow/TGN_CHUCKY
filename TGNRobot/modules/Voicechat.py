f611138aab73b0e9322e0b5d24c5758282df9196
Kittuthemeow <89501967+KIRANRAJ6554@users.noreply.github.com>
2021-11-09 23:14:05 +0530

    Update Voicechat.py

--- TGNRobot/modules/Voicechat.py
+++ TGNRobot/modules/Voicechat.py
@@ -27,14 +27,14 @@ async def ai_lycia(url):
     return ai_name
 
 
-@LYCIA.on_message(filters.command("tiana"))
+@LYCIA.on_message(filters.command("vc"))
 async def Lycia(_, message):
     if len(message.command) < 2:
         await message.reply_text("Tiana AI Voice Chatbot")
         return
     text = message.text.split(None, 1)[1]
     lycia = text.replace(" ", "%20")
-    m = await message.reply_text("tiana Is Best...")
+    m = await message.reply_text("chucky the mass😎😎...")
     try:
         L = await fetch(f"https://api.affiliateplus.xyz/api/chatbot?message={lycia}&botname=Emcee&ownername=Pranav&user=1")
         chatbot = L["message"]
@@ -43,9 +43,9 @@ async def Lycia(_, message):
     except Exception as e:
         await m.edit(str(e))
         return
-    await m.edit("Made By Prince Prajapati...")
+    await m.edit("wait pannunga nan audio record panitu eruka...")
     LyciaVoice = await ai_lycia(VoiceAi)
-    await m.edit("Repyping...")
+    await m.edit("connecting to chucky server...")
     await message.reply_audio(audio=LyciaVoice, title=chatbot, performer=name)
     os.remove(LyciaVoice)
     await m.delete()

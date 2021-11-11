981205c41a2c7382198cadf739842d2c11a658a6
Kittuthemeow <89501967+KIRANRAJ6554@users.noreply.github.com>
2021-11-11 07:49:00 +0530

    Update Img-`edit`.py

--- TGNRobot/modules/Img-`edit`.py
+++ TGNRobot/modules/Img-`edit`.py
@@ -56,7 +56,7 @@ from ADDN.edit_5 import (  # pylint:disable=import-error
     scanlineglitch_4,
     scanlineglitch_5,
 )
-from ADDN import chuky as Client
+from ADDN.chuky import chuky as Client
 
 lel = 00000000
 # pylint:disable=import-error

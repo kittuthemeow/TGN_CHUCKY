91335fd366ee21e8e7c0c1b3d93ff2204cb35c8f
Hmmm <89540820+Mohan745@users.noreply.github.com>
2021-11-13 12:47:43 +0530

    Update __main__.py

--- TGNRobot/__main__.py
+++ TGNRobot/__main__.py
@@ -699,7 +699,7 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "[Chucky Back Online (:](https://telegra.ph/file/2c653032f76b39ba285e1.jpg)"
+            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "[Chucky Back Online (:](https://telegra.ph/file/2c653032f76b39ba285e1.jpg)",
         except Unauthorized:
             LOGGER.warning(
                 "Bot isnt able to send message to support_chat, go and check!"

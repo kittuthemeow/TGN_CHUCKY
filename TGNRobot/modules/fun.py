15388bc8eded17f0dce65b8819249327c5d8185c
ValtAoiTheBot <91861970+ValtAoiTheBot@users.noreply.github.com>
2021-11-13 15:38:17 +0530

    Update fun.py

--- TGNRobot/modules/fun.py
+++ TGNRobot/modules/fun.py
@@ -12,6 +12,8 @@ from TGNRobot.modules.helper_funcs.extraction import extract_user
 from telegram import ChatPermissions, ParseMode, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, run_async, CommandHandler, Filters
+from telegram.utils.helpers import escape_markdown
+
 
 import TGNRobot.modules.helper_funcs.string_store as fun
 

73ad3164ef3ae4fee54614f32d9eaa72d31c519b
Kittuthemeow <89501967+KIRANRAJ6554@users.noreply.github.com>
2021-11-28 21:46:09 +0530

    Update bans.py

--- TGNRobot/modules/bans.py
+++ TGNRobot/modules/bans.py
@@ -11,7 +11,7 @@ from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
 from telegram.utils.helpers import mention_html
 
-from VegetaRobot import (
+from TGNRobot import (
     DEV_USERS,
     LOGGER,
     OWNER_ID,
@@ -21,9 +21,9 @@ from VegetaRobot import (
     WOLVES,
     dispatcher,
 )
-import VegetaRobot.modules.sql.users_sql as sql
-from VegetaRobot.modules.disable import DisableAbleCommandHandler
-from VegetaRobot.modules.helper_funcs.chat_status import (
+import TGNRobot.modules.sql.users_sql as sql
+from TGNRobot.modules.disable import DisableAbleCommandHandler
+from TGNRobot.modules.helper_funcs.chat_status import (
     user_admin_no_reply,
     bot_admin,
     can_restrict,
@@ -35,9 +35,9 @@ from VegetaRobot.modules.helper_funcs.chat_status import (
     user_can_ban,
     can_delete,
 )
-from VegetaRobot.modules.helper_funcs.extraction import extract_user_and_text
-from VegetaRobot.modules.helper_funcs.string_handling import extract_time
-from VegetaRobot.modules.log_channel import gloggable, loggable
+from TGNRobot.modules.helper_funcs.extraction import extract_user_and_text
+from TGNRobot.modules.helper_funcs.string_handling import extract_time
+from TGNRobot.modules.log_channel import gloggable, loggable
 
 
 @run_async

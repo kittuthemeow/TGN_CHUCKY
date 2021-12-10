2c1fff58e182aac2ad3e036b9429a98e4bc3f312
TheTeleGramDon <89540820+TheTeleGramDon@users.noreply.github.com>
2021-12-10 20:56:44 +0530

    Update sample_config.py

--- TGNRobot/sample_config.py
+++ TGNRobot/sample_config.py
@@ -1,72 +1,50 @@
-# Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
 import json
 import os
-
 
 def get_user_list(config, key):
     with open("{}/TGNRobot/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
-
-# Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
-class Config(object):
-    LOGGER = True
-    # REQUIRED
-    # Login to https://my.telegram.org and fill in these slots with the details given by it
-
-    API_ID = 123456  # integer value, dont use ""
-    API_HASH = "boom"
-    TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 1669178360  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Timesisnotwaiting" 
-    SUPPORT_CHAT = "GodfatherSupport"  # Your own group for support, do not add the @
-    JOIN_LOGGER = (
-        -1001392103597
-    )  # Prints any new group the bot is added to, prints just the name and ID.
-    EVENT_LOGS = (
-        -1001392103597
-    )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
-
-    # RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = "something://somewhat:user@hosturl:port/databasename"  # needed for any database modules
-    LOAD = []
-    NO_LOAD = ["rss", "cleaner", "connection", "math"]
-    WEBHOOK = False
-    INFOPIC = True
-    URL = None
-    SPAMWATCH_API = ""  # go to support.spamwat.ch to get key
-    SPAMWATCH_SUPPORT_CHAT = "SpamWatchSupport"
-
-    # OPTIONAL
-    ##List of id's -  (not usernames) for users which have sudo access to the bot.
-    DRAGONS = get_user_list("elevated_users.json", "sudos")
-    ##List of id's - (not usernames) for developers who will have the same perms as the owner
-    DEV_USERS = get_user_list("elevated_users.json", "devs")
-    ##List of id's (not usernames) for users which are allowed to gban, but can also be banned.
-    DEMONS = get_user_list("elevated_users.json", "supports")
-    # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
-    TIGERS = get_user_list("elevated_users.json", "tigers")
-    WOLVES = get_user_list("elevated_users.json", "whitelists")
-    DONATION_LINK = None  # EG, paypal
-    CERT_PATH = None
-    PORT = 5000
-    DEL_CMDS = True  # Delete commands that users dont have access to, like delete /ban if a non admin uses it.
-    STRICT_GBAN = True
-    WORKERS = (
-        8  # Number of subthreads to use. Set as number of threads your processor uses
-    )
-    BAN_STICKER = ""  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
-    ALLOW_EXCL = True  # Allow ! commands as well as / (Leave this to true so that blacklist can work)
-    CASH_API_KEY = (
-        "awoo"  # Get your API key from https://www.alphavantage.co/support/#api-key
-    )
-    TIME_API_KEY = "zaid"  # Get your API key from https://timezonedb.com/api
-    WALL_API = (
-        "awoo"  # For wallpapers, get one from https://wall.alphacoders.com/api.php
-    )
-    AI_API_KEY = "awoo"  # For chatbot, get one from https://coffeehouse.intellivoid.net/dashboard
-    BL_CHATS = []  # List of groups that you want blacklisted.
-    SPAMMERS = None
+LOGGER = "True"
+ALLOW_EXCL = ""
+BL_CHATS = ""
+BAN_STICKER = ""
+BOT_ID = "2003043761"
+BOT_USERNAME = "@Chuckymusic_bot"
+CASH_API_KEY = ""
+DATABASE_URL = 
+DEL_CMDS = "true"
+DEMONS  = "2057139792 1993128405 1181494383 1393492535 1904184235 1234465167 1480007084 1912630709
+2117266789 1044655712"
+DEV_USERS = "2057139792 1993128405 1181494383 1393492535 1904184235 1234465167 1480007084 1912630709
+2117266789 1044655712"
+ENV = ""
+DRAGONS = "2057139792 1993128405 1181494383 1393492535 1904184235 1234465167 1480007084 1912630709
+2117266789 1044655712"
+EVENT_LOGS = "-1001570400013"
+INFOPIC = "true"
+JOIN_LOGGER = "-1001570400013"
+MONGO_DB_URI = "mongodb+srv://Izazkhan:izazkhan@cluster0.hlltt.mongodb.net/project0?retryWrites=true&w=majority"
+No_LOAD = "true"
+OPENWEATHERMAP_API=
+OWNER_ID = "2068952393"
+OWNER_USERNAME = "@Kittu_the_criminal"
+PORT= ""
+REM_BG_API_KEY= "dxsh728mZMDmj4ijSZCNPZig"
+SQLALCHEMY_DATABASE_URI = "sqldbtype://username:pw@hostname:port/db_name"
+STRICT_GBAN = "true"
+STRICT_GMUTE = "true"
+SUPPORT_CHAT = "chucky_support"
+SW_API=
+TIGERS ="2057139792 1993128405 1181494383 1393492535 1904184235 1234465167 1480007084 1912630709
+2117266789 1044655712"
+TIME_API_KEY = ""
+URL = ""
+TOKEN = "2003043761:AAHCRFosoRfjJMrcsJpDrGwnOTQddWG3Ul8"
+WALL_API = ""
+WEBHOOK = ""
+WOLVES = "2057139792 1993128405 1181494383 1393492535 1904184235 1234465167 1480007084 1912630709
+2117266789 1044655712"
 
 class Production(Config):
     LOGGER = True

